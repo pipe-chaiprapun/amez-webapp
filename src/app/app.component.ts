7d0b4a16df7e2665c0aef4d8b93e9ed2d5315192
writeone <chaiprapun@hotmail.com>
2021-03-21 01:54:50 +0700

    - add ngx bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare const bootstrap;
+declare const $;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'amez-app';
+export class AppComponent implements OnInit {
+  title = 'AMEZ';
+
+  ngOnInit() {
+    $('.marquee').marquee({
+      duration: 20000,
+      gap: 500,
+      delayBeforeStart: 0,
+      direction: 'left',
+      duplicated: true
+    });
+
+  }
 }
