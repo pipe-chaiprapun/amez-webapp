27f92aa28105f714cf57549fef13f51e9090f422
Nuttapat <chaiprapun@hotmail.com>
2021-03-30 15:01:53 +0700

    - update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,11 @@ declare const $;
 })
 export class AppComponent implements OnInit {
   title = 'AMEZ';
+  rollerImg = '/assets/Images/step/roller1.png';
+  serumImg = '/assets/Images/step/serum1.png';
+
+  roller = true;
+  serum = true;
 
   ngOnInit() {
     $('.marquee').marquee({
@@ -17,6 +22,28 @@ export class AppComponent implements OnInit {
       direction: 'left',
       duplicated: true
     });
+  }
 
+  changeRollerImg() {
+    this.roller = !this.roller;
+
+    // if (this.roller) {
+    //   $('#roller2').fadeOut(500, () => {
+    //     $('#roller1').fadeIn(500);
+    //   });
+    // } else {
+    //   $('#roller1').fadeOut(500, () => {
+    //     $('#roller2').fadeIn(500);
+    //   });
+    // }
+    $('#roller').fadeOut(1000, () => {
+      this.rollerImg = this.roller ? '/assets/Images/step/roller1.png' : '/assets/Images/step/roller2.png';
+    }).fadeIn(1000);
+  }
+  changeSerumImg() {
+    this.serum = !this.serum;
+    $('#serum').fadeOut(1000, () => {
+      this.serumImg = this.serum ? '/assets/Images/step/serum1.png' : '/assets/Images/step/serum2.png';
+    }).fadeIn(1000);
   }
 }
