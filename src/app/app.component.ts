8da7a77c4bcce8726daa0df39d0db94051b467fb
Nuttapat <chaiprapun@hotmail.com>
2021-04-27 14:53:37 +0700

    - refactory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,28 @@ export class AppComponent implements OnInit {
   title = 'AMEZ';
   rollerImg = '/assets/Images/step/roller1.png';
   serumImg = '/assets/Images/step/serum1.png';
+  prop1 = '/assets/Images/properties/prop1.png';
+  prop2 = '/assets/Images/properties/prop2.png';
+  prop3 = '/assets/Images/properties/prop3.png';
+  prop4 = '/assets/Images/properties/prop4.png';
+  btnCheck = '/assets/Images/contact/checkPrice.png';
+  btnAsk = '/assets/Images/contact/askBtn2.png';
+  btnLine = '/assets/Images/contact/addLineBtn.png';
+  btnPrice1 = 'assets/Images/productPrice/price1.png';
+  btnPrice2 = 'assets/Images/productPrice/price2.png';
+  btnPromo1 = '/assets/Images/promotion/price1.png';
+  btnPromo2 = '/assets/Images/promotion/price2.png';
+  btnPromo3 = '/assets/Images/promotion/price3.png';
 
   roller = true;
   serum = true;
 
   ngOnInit() {
+    this.initJquery();
+
+  }
+
+  initJquery() {
     $('.marquee').marquee({
       duration: 20000,
       gap: 500,
@@ -22,6 +39,73 @@ export class AppComponent implements OnInit {
       direction: 'left',
       duplicated: true
     });
+    $('.carousel').carousel({ interval: 3000 });
+    $('#prop1').hover(() => {
+      this.prop1 = '/assets/Images/properties/prop12.png';
+    }, () => { this.prop1 = '/assets/Images/properties/prop1.png'; });
+    $('#prop2').hover(() => {
+      this.prop2 = '/assets/Images/properties/prop22.png';
+    }, () => { this.prop2 = '/assets/Images/properties/prop2.png'; });
+    $('#prop3').hover(() => {
+      this.prop3 = '/assets/Images/properties/prop32.png';
+    }, () => { this.prop3 = '/assets/Images/properties/prop3.png'; });
+    $('#prop4').hover(() => {
+      this.prop4 = '/assets/Images/properties/prop42.png';
+    }, () => { this.prop4 = '/assets/Images/properties/prop4.png'; });
+    $('#btn-check').hover(() => {
+      this.btnCheck = '/assets/Images/contact/clickBtn.png';
+    }, () => { this.btnCheck = '/assets/Images/contact/checkPrice.png'; });
+    $('#btn-ask').hover(() => {
+      this.btnAsk = '/assets/Images/contact/clickBtn.png';
+    }, () => { this.btnAsk = '/assets/Images/contact/askBtn2.png'; });
+    $('#btn-line').hover(() => {
+      this.btnLine = '/assets/Images/contact/askBtn.png';
+    }, () => { this.btnLine = '/assets/Images/contact/addLineBtn.png'; });
+    $('#product-price1').hover(() => {
+      this.btnPrice1 = 'assets/Images/productPrice/price3.png';
+    }, () => { this.btnPrice1 = 'assets/Images/productPrice/price1.png'; });
+    $('#product-price2').hover(() => {
+      this.btnPrice2 = 'assets/Images/productPrice/price3.png';
+    }, () => { this.btnPrice2 = 'assets/Images/productPrice/price2.png'; });
+    $('#btn-promo1').hover(() => {
+      this.btnPromo1 = '/assets/Images/promotion/price4.png';
+    }, () => { this.btnPromo1 = '/assets/Images/promotion/price1.png'; });
+    $('#btn-promo2').hover(() => {
+      this.btnPromo2 = '/assets/Images/promotion/price4.png';
+    }, () => { this.btnPromo2 = '/assets/Images/promotion/price2.png'; });
+    $('#btn-promo3').hover(() => {
+      this.btnPromo3 = '/assets/Images/promotion/price4.png';
+    }, () => { this.btnPromo3 = '/assets/Images/promotion/price3.png'; });
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() >= 450) {
+        $('.scroll-top').fadeIn();
+      } else {
+        $('.scroll-top').fadeOut();
+      }
+    });
+    $('.scroll-top').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
+
+  }
+
+  gotoStep() {
+    // console.log('go')
+    // $('#step-section').animate({ scrollTop: 0 }, 'slow');
+    $('html, body').animate({
+      scrollTop: $('#step-section').offset().top
+    }, 1500);
+  }
+  gotoPromotion() {
+    $('html, body').animate({
+      scrollTop: $('#promotion-section').offset().top
+    }, 1500);
+  }
+  gotoContact() {
+    $('html, body').animate({
+      scrollTop: $('#contact-section').offset().top
+    }, 1500);
   }
 
   changeRollerImg() {
